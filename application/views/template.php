780d609de365cdf50aef9f3d3830990baa6ef169
Steven Tsao <steven.tsao777@gmail.com>
2017-01-27 11:20:41 -0800

    Completed website conversion

--- application/views/template.php
+++ application/views/template.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <title>The Gallery</title>
+        <title>{pagetitle}</title>
         <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
-        <link rel="stylesheet" type="text/css" media="all" href="css/reset.css" />
-        <link rel="stylesheet" type="text/css" media="all" href="css/text.css" />
-        <link rel="stylesheet" type="text/css" media="all" href="css/style.css" />
-        <link rel="stylesheet" type="text/css" media="all" href="css/lightbox.css" />
+        <link rel="stylesheet" type="text/css" media="all" href="/css/reset.css" />
+        <link rel="stylesheet" type="text/css" media="all" href="/css/text.css" />
+        <link rel="stylesheet" type="text/css" media="all" href="/css/style.css" />
+        <link rel="stylesheet" type="text/css" media="all" href="/css/lightbox.css" />
     </head>
     <body>
         <div id="wrapper">
